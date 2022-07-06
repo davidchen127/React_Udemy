f429e13aa7e9e90a21d7cf2743459e50608697a6
ch.chen <davidchen127@outlook.de>
2022-07-06 17:25:56 +0200

    progress

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,13 +1,17 @@
+import { useState } from "react"
 import Header from "./components/Header"
+import FeedbackData from "./data/FeedbackData"
+import FeedbackList from "./components/FeedbackList"
 
 function App() {
+    const [feedback, setfeedback] = useState(FeedbackData);
 
     
  return (
-    <>
+     <>
         <Header  text={'Feedback UI'}></Header>
         <div className='container'>
-
+        <FeedbackList feedback={feedback}/>
         </div>
     </>
   )
---
+++ feedback-app/src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 7,
+    text: 'i like this product',
+  },
+  {
+    id: 2,
+    rating: 3,
+    text: 'it is basically garbage',
+  },
+  {
+    id: 3,
+    rating: 9,
+    text: 'absolutely love it',
+  },
+]
+
+export default FeedbackData
