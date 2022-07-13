8a5bcc97d45ce3a6e8a96e935ba67f07f5c1ca41
ch.chen <davidchen127@outlook.de>
2022-07-13 12:35:24 +0200

    useRef

--- advanced-hooks/src/App.js
+++ advanced-hooks/src/App.js
@@ -1,5 +1,13 @@
+import UseRefExampl1 from './components/UseRefExample1'
+import UseRefExampl2 from './components/UseRefExample2'
+
 function App() {
-  return <div className='container mt-5'></div>
+  return (
+    <div className='container mt-5'>
+      {/* <UseRefExampl1/> */}
+      <UseRefExampl2 />
+    </div>
+  )
 }
 
 export default App
